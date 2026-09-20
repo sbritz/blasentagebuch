0b98b5438bbf06bde9a2d6c529e9e2813fdc3a91
sbritz <s.britz@cineprog.de>
2026-09-20 09:33:22 +0200

    Allow selecting vessel photos from library

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "blasentagebuch-v26";
-const APP_SHELL = ["./", "./index.html", "./app.css?v=26", "./app.js?v=26", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+const CACHE = "blasentagebuch-v27";
+const APP_SHELL = ["./", "./index.html", "./app.css?v=27", "./app.js?v=27", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
